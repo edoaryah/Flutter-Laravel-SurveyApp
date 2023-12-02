c52835339a03ba5a33d628a2783590fb8680eaf6
Edo Arya Hermawan <edoarya2002@gmail.com>
2023-12-02 15:02:48 +0700

    Update endpoint.dart

--- flutter_survey_dashboard/lib/services/endpoint.dart
+++ flutter_survey_dashboard/lib/services/endpoint.dart
@@ -1,6 +1,6 @@
 class Endpoints {
   // static const String urlDomain = 'http://192.168.77.204:8000/';
-  static const String urlDomain = 'http://192.168.77.221:8000/'; //punya diki ya
+  static const String urlDomain = 'http://192.168.64.85:8000/'; //punya diki ya
 
   static const String totalRespondents = 'api/total-respondents';
   static const String respondentsByGender = 'api/respondents-by-gender';
